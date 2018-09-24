176a73a98054d321a21e0877ee2fb17c64082d78
AlxDima <alex.m.dima@gmail.com>
2018-09-24 14:32:20 +0200

    using vim to code

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Project
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-        }
+            Console.WriteLine("Using VIM to code .NET")
+	}
     }
 }
